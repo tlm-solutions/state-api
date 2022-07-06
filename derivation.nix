d721ae04ca2591aed345dd47aa5d608fa47a28c6
revol-xut <revol-xut@protonmail.com>
2022-07-06 10:48:28 +0200

    fix nix build

--- derivation.nix
+++ derivation.nix
@@ -1,14 +1,14 @@
-{ naersk, src, lib, pkg-config, cmake, protobuf }:
+{ naersk, src, lib, pkg-config, cmake, protobuf, postgresql, zlib }:
 
 naersk.buildPackage {
   pname = "dvb-api";
-  version = "0.1.0";
+  version = "0.1.1";
 
   src = ./.;
 
   cargoSha256 = lib.fakeSha256;
 
-  nativeBuildInputs = [ pkg-config protobuf cmake ];
+  nativeBuildInputs = [ pkg-config protobuf cmake postgresql zlib ];
 
   meta = with lib; {
     description = "public api to fetch all the spicy live data";
